6337f0d5b7b67c2c81d1afe9748322e610f60d98
SBHattarj <subhambhsubhambh@gmail.com>
2025-02-16 00:18:22 +0530

    removed unnecesarry files

--- dist/db/db.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-try {
-    await mongoose.connect(process.env.MONGODB_URL);
-    console.log("Connected to MongoDB");
-}
-catch (error) {
-    console.log("Failed to connect to MongoDB", error);
-}
